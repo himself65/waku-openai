1784a224023892888b39671d0bfebb3465965510
Alex Yang <himself65@outlook.com>
2024-01-19 21:19:59 -0600

    fix: server action called on client

--- src/actions/chat.tsx
+++ src/actions/chat.tsx
@@ -1,49 +1,8 @@
 'use server'
-import OpenAI from 'openai'
 import { getEnv } from 'waku'
 
-const apiKey = getEnv('OPENAI_API_KEY')!
-
-export const openai = new OpenAI({
-  apiKey
-})
-
-type ContentProps = {
-  reader: ReadableStreamDefaultReader<any>
-}
-
-const Content = async (props: ContentProps) => {
-  const reader = props.reader
-  const content = await reader.read()
-  if (content.value === undefined) {
-    return null
-  }
-  const value = JSON.parse(
-    Buffer.from(content.value).toString('utf-8')).choices[0]?.delta?.content
-  console.log('v', value)
-  const done = content.done
-  return (
-    <>
-      <>
-        {value}
-      </>
-      {
-        done ? null : <Content reader={reader}/>
-      }
-    </>
-  )
-}
+const apiKey = getEnv('A')!
 
 export async function generateResponse () {
-  const stream = await openai.chat.completions.create({
-    messages: [
-      {
-        role: 'system',
-        content: 'Hello, I am a chatbot that can answer questions about the Waku framework.'
-      }
-    ],
-    model: 'gpt-3.5-turbo',
-    stream: true
-  })
-  return <Content reader={stream.toReadableStream().getReader()}/>
+  return `apiKey: ${apiKey}`
 }
